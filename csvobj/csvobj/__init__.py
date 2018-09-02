0048c90c635ab9ea38508d97638f836888bc751f
Olivier Rey <rey.olivier@gmail.com>
2018-09-02 10:48:25 +0200

    adding utility

--- csvobj/csvobj/__init__.py
+++ csvobj/csvobj/__init__.py
@@ -1,10 +1,2 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-
-env = Environment(
-    loader=PackageLoader('csvobj', 'templates'),
-    autoescape=select_autoescape(['py'])
-)
-
 __all__ = [testf]
 
-test = True
