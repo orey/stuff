73db444d3e86847fc621adb1bff2bfc770d5e9fa
Olivier Rey <rey.olivier@gmail.com>
2018-09-08 09:15:41 +0200

    Adding multiple hashing alsorithms + test function + management in full names + list of folders

--- sorter/sorter.py
+++ sorter/sorter.py
@@ -8,6 +8,7 @@
 import hashlib
 import sys
 import time
+import os
 
 from os import listdir
 from os.path import isfile, join
@@ -15,55 +16,127 @@ from os.path import isfile, join
 
 FOLDER = '/home/olivier/.aMule/Incoming/'
 EXT = '.pdf'
+MINSIZE = 5000
+MAXSIZE = 15000000
 
-DICT = {}
-#-- Default hasher
-HASHER = hashlib.md5()
+VERBOSE = True
+
+
+def convertBytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+def fileOK(completename):
+    """
+    Filters the files not to generate hash for too small or too big
+    files or for files with the wrong extension
+    """
+    if not completename.endswith(EXT):
+        return False
+    fsize = os.stat(completename).st_size
+    if fsize > MAXSIZE:
+        if VERBOSE: print("== File: " + completename + " too big (" + \
+                         convertBytes(fsize) + "). Excluding.")
+        return False
+    elif fsize < MINSIZE:
+        if VERBOSE: print("== File: " + completename + " too small (" + \
+                         convertBytes(fsize) + "). Excluding.")
+        return False
+    else:
+        return True
+
 
 def getFilesInFolder(mypath):
-    return [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    """
+    Returns two lists, the list of files and the list of subfolders
+    All is managed with complete names
+    The list of files is filtered
+    """
+    glob = listdir(mypath)
+    files = []
+    folders = []
+    for f in glob:
+        completef = join(mypath,f)
+        if isfile(completef):
+            if fileOK(completef):
+                files.append(completef)
+        else:
+            folders.append(completef)
+    return files, folders
 
 
-def getHash(myfile):
-    hasher = HASHER
+def testGetFilesInFolder():
+    files, folders = getFilesInFolder('/home/olivier/.aMule/Incoming')
+    print("== Files:")
+    print(files)
+    print("== Folders:")
+    print(folders)
+
+def getHash(myfile, algo=0):
+    hasher = None
+    if algo == 1:
+        hasher = hashlib.sha1()
+    else:
+        hasher = hashlib.md5()
     with open(myfile, 'rb') as afile:
         buf = afile.read()
         hasher.update(buf)
     return hasher.hexdigest()
 
 
-def main():
-    files = getFilesInFolder(FOLDER)
-    print("== Folder: " + FOLDER)
-    print("== " + str(len(files)) + " files found")
+def createDict(folder, algo=0):
+    """
+    Creates a dict with a hash and the file in order to spot the duplicate files
+    even if they don't have the same names
+    """
+    dict = {}
+    files, folders = getFilesInFolder(folder)
+    if VERBOSE:
+        print("== Folder: " + folder)
+        print("== " + str(len(files)) + " files found with matching filter")
     dupes = 0
     keys = 0
-    for f in files:
-        completename = FOLDER + f
-        h = getHash(completename)
+    for completename in files:
+        h = getHash(completename, algo)
         keys +=1
         sys.stdout.write(str(keys) + "|")
         sys.stdout.flush()
         try:
-            temp = DICT[h]
-            print('\n== Found duplicate of ' + "'" + completename + "'")
-            print("== '" + temp + "'")
+            temp = dict[h]
+            if VERBOSE:
+                print('\n== Found duplicate of ' + "'" + completename + "'")
+                print("== '" + temp + "'")
             dupes += 1
         except KeyError:
-            DICT[h] = completename
-    print("\n== Generated " + str(len(DICT)) + " MD5 keys")
+            dict[h] = completename
+    return dict
 
-if __name__ == "__main__":
-    start = time.time()
-    main()
-    end = time.time()
-    print("== Execution time with md5: " + str(end - start))
-    HASHER = hashlib.sha1()
-    start = time.time()
-    main()
-    end = time.time()
-    print("== Execution time with sha1: " + str(end - start))
+def compareHash():
+    start1 = time.time()
+    dict1 = createDict('/home/olivier/.aMule/Incoming')
+    end1 = time.time()
+
+    start2 = time.time()
+    dict2 = createDict('/home/olivier/.aMule/Incoming', 1)
+    end2 = time.time()
+
+    print("\n== Generated " + str(len(dict1)) + " MD5 keys")
+    print("== Execution time with md5: " + str(end1 - start1))
+    print("\n== Generated " + str(len(dict2)) + " SHA1 keys")
+    print("== Execution time with sha1: " + str(end2 - start2))
     
+
+    
+if __name__ == "__main__":
+    #main()
+    #testGetFilesInFolder()
+    compareHash()
 
 
 
