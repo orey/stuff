610770a411d4a8ec171deefa5f67cbf747035bb3
Olivier Rey <rey.olivier@gmail.com>
2018-09-13 11:36:26 +0200

    Initial commit

---
+++ freeplane/template.mm
@@ -0,0 +1,141 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node STYLE_REF="Titre1" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1536830847342"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Title1
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle" zoom="1.21">
+    <properties fit_to_viewport="false;" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="Titre1" COLOR="#000000" BACKGROUND_COLOR="#99ccff" STYLE="bubble">
+<font NAME="Calibri" SIZE="20" BOLD="true"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode TEXT="Titre2" BACKGROUND_COLOR="#ccccff" STYLE="bubble">
+<font NAME="Calibri" SIZE="18" BOLD="true"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode TEXT="Titre3" BACKGROUND_COLOR="#ccffcc">
+<font NAME="Calibri" SIZE="16" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode TEXT="Titre4" COLOR="#000099" BACKGROUND_COLOR="#ffffcc">
+<font NAME="Calibri" SIZE="14" BOLD="true"/>
+</stylenode>
+<stylenode TEXT="Titre5" COLOR="#003399" BACKGROUND_COLOR="#ccffff">
+<font NAME="Calibri" SIZE="12" BOLD="true" ITALIC="false"/>
+</stylenode>
+<stylenode TEXT="NormalPerso" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font NAME="Calibri" SIZE="10"/>
+</stylenode>
+<stylenode TEXT="Normal-Red" COLOR="#ff3333">
+<font NAME="Calibri" SIZE="10"/>
+</stylenode>
+<stylenode TEXT="URGENT" COLOR="#ffff99" BACKGROUND_COLOR="#ff3333">
+<font NAME="Calibri" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode TEXT="IMPORTANT" COLOR="#ffffff" BACKGROUND_COLOR="#ffcc33">
+<font NAME="Calibri" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="BoldItalic-Green" BACKGROUND_COLOR="#ccffcc">
+<font SIZE="8" BOLD="true" ITALIC="true"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+<stylenode TEXT="Bold-Violet" BACKGROUND_COLOR="#ccccff" STYLE="bubble">
+<font SIZE="9" BOLD="true"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+<stylenode TEXT="Bold-Blue" BACKGROUND_COLOR="#99ccff">
+<font BOLD="true"/>
+</stylenode>
+<stylenode TEXT="Normal" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="10"/>
+</stylenode>
+<stylenode TEXT="Question" COLOR="#000099" BACKGROUND_COLOR="#ffffcc">
+<font NAME="Liberation Sans" SIZE="8" BOLD="true"/>
+</stylenode>
+<stylenode TEXT="small-item" BACKGROUND_COLOR="#ccffff">
+<font SIZE="8"/>
+</stylenode>
+<stylenode TEXT="Action-Importante" COLOR="#ffffcc" BACKGROUND_COLOR="#ff0033">
+<icon BUILTIN="messagebox_warning"/>
+<font BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<font NAME="Arial"/>
+<node TEXT="Title2" STYLE_REF="Titre2" POSITION="right" ID="ID_684589388" CREATED="1524221164684" MODIFIED="1536831322018">
+<font NAME="Arial"/>
+<node TEXT="Title3" STYLE_REF="Titre3" ID="ID_1177474624" CREATED="1536829638249" MODIFIED="1536831328160">
+<node TEXT="Title4" STYLE_REF="Titre4" ID="ID_1815166023" CREATED="1536830222476" MODIFIED="1536830233313">
+<node TEXT="Title5" STYLE_REF="Titre5" ID="ID_1900135035" CREATED="1536830235060" MODIFIED="1536830243308">
+<node TEXT="Normal" STYLE_REF="NormalPerso" ID="ID_133865499" CREATED="1536830245993" MODIFIED="1536830777347"/>
+<node TEXT="Normal-Red" STYLE_REF="Normal-Red" ID="ID_918928275" CREATED="1536830777347" MODIFIED="1536830835726"/>
+<node TEXT="Urgent" STYLE_REF="URGENT" ID="ID_899099703" CREATED="1536830856875" MODIFIED="1536831311800"/>
+<node TEXT="Important" STYLE_REF="IMPORTANT" ID="ID_217160763" CREATED="1536830873189" MODIFIED="1536831316411"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
