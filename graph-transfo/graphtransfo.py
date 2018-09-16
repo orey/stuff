02cd72184c107a8f24ced951b37e23ed2a9677a2
Olivier Rey <rey.olivier@gmail.com>
2018-09-16 21:48:49 +0200

    basic init

---
+++ graph-transfo/graphtransfo.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Name:        graphtransfo.py
+# Purpose:     Graph transformation implementation
+#
+# Author:      O. Rey
+#
+# Created:     September 16 2018
+# Copyleft:    GNU GPL v3
+#-------------------------------------------------------------------------------
