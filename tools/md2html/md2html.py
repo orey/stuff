7b0ac3e4a452888b34ea0907e2b49c18ce82bb53
Olivier Rey <rey.olivier@gmail.com>
2019-01-28 08:53:31 +0100

    new version

--- tools/md2html/md2html.py
+++ tools/md2html/md2html.py
@@ -1,14 +1,26 @@
 #!/bin/python3
-import markdown
+import os, markdown, codecs
 
+MYPATH = "C:/Users/Toto/path/to/minutes/"
 MINUTES = "minutes.md"
 
 if __name__ ==  "__main__":
-    input_file = codecs.open(MINUTES, mode="r", encoding="utf-8")
+    fulldirname = None
+    while True:
+        folder = input("Name of the folder: ")
+        fulldirname = os.path.join(MYPATH, folder)
+        if not os.path.isdir(fulldirname):
+            print('"' + fulldirname + '" is not a folder.')
+        else:
+            break
+    fullfilename = os.path.join(fulldirname, MINUTES)
+    input_file = codecs.open(fullfilename, mode="r", encoding="utf-8")
     text = input_file.read()
     html = markdown.markdown(text)
-    output_file = codecs.open("some_file.html", "w",
+    output_file = codecs.open(os.path.join(fulldirname, "minutes.html"),
+                              "w",
                               encoding="utf-8",
                               errors="xmlcharrefreplace")
     output_file.write(html)
+    print("File generated")
 
