99eefc12a8b3ed64ea468c7834d5b4e1cde0c781
Olivier Rey <rey.olivier@gmail.com>
2019-03-20 15:35:38 +0100

    adding generator

---
+++ tools/generate-md-list-of-images/generate.py
@@ -0,0 +1,25 @@
+import os, sys
+from os import listdir
+from os.path import isfile, join
+
+print('== Generates a md file that points to all images ==')
+
+mypath = os.getcwd()
+
+relativedir = mypath.split('\\')[-1]
+fname = relativedir.replace(" ","") + ".md"
+
+print("File name: " + fname)
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+count = 0
+with open(fname, 'w') as fil:
+    fil.write("# " + relativedir + "\n\n")
+    for f in onlyfiles:
+        if f.endswith('.PNG') or f.endswith('.png'):
+            count += 1
+            fil.write("[Image" + str(count) + "](" + f + ")\n\n")
+            print(f)
+fil.close()
+
