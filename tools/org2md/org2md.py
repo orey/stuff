b48cf90d153bee442354b806d8706f7b89478bca
Olivier Rey <rey.olivier@gmail.com>
2020-01-24 17:59:09 +0100

    new version

--- tools/org2md/org2md.py
+++ tools/org2md/org2md.py
@@ -2,40 +2,103 @@
 
 #import sys, os, markdown, codecs
 
-import sys
+import sys, codecs
 
 from pathlib import Path
 
 MYPATH = "C:/Users/a876246/Documents/oreyboulot-Airbus/Airbus Helicopters/__minutes/"
 
-RULE_HEAD = {"* ": "# ", "** ": "## ", "*** ": "### ", "**** ": "#### ", "***** ": "##### ", "-":"*", "  -":"  *", "    -":"    *"}
+#--- key:org, value:md
+#--- Start of line
+RULE_HEAD = {"* ": "# ",
+             "** ": "## ",
+             "*** ": "### ",
+             "**** ": "#### ",
+             "***** ": "##### ",
+             "-":"*",
+             "  -":"  *",
+             "    -":"    *"}
 
-#--- Underline, bold, italics, link
-RULE_LINE = {"_|_":"|", "*|*":"**|**", "/|/":"_|_", "[[|]]":"!(|)"}
+#--- Underline, bold, italics
+#--- symetrical rules
+#--- key:org, value:md
+RULE_LINE = {"_":"",
+             "*":"**",
+             "/":"_"}
+
+#--- unsymetrical rule
+RULE_LINK = {"[[|]]":"!(|)",
+             "file:|":()}
 
 
-def convert_line(str):
+def convert_line(stri):
     newstr = ""
     cursor = 0
     for key in RULE_HEAD:
-        if str.startswith(key):
-            newstr = RULE_HEAD[key]
-            cursor = len(newstr)
-    tempstr = str[cursor:]
+        if stri.startswith(key):
+            newstr = stri.replace(key, RULE_HEAD[key])
+            return newstr
+    newstr = stri
     for key in RULE_LINE:
-        rule = key.split("|")
-        if tempstr.startswith(rule[0]) and tempstr[len(begin)]!=" ":
-            #search for the end
+        le = len(key)
+        inds = newstr.find(key)
+        inde = newstr.find(key, inds+le)
+        if inds != -1 \
+           and inde !=-1 \
+           and newstr[inds+le] != " " \
+           and newstr[inde-le] != " " \
+           and (inds == 0 or newstr[inds-1] == " ") \
+           and (inde == len(newstr)-1 or newstr[inde+le]==" "):
+            rep = RULE_LINE[key]
+            newstr = newstr.replace(key, rep, 2)
+    for key in RULE_LINK:
+        bkey = key.split("|")[0]
+        leb = len(bkey)
+        ekey = key.split("|")[1]
+        lee = len(ekey)
+        inds = newstr.find(bkey)
+        inde = newstr.find(ekey, inds+1)
+        if inds != -1 and inde !=-1 \
+           and newstr[inds+leb] != " " \
+           and newstr[inde-1] != " " \
+           and (inds == 0 or newstr[inds-1] == " ") \
+           and (inde == len(newstr)-lee or newstr[inde+lee]==" "):
+            newstr = newstr.replace(bkey, RULE_LINK[key].split("|")[0])
+            newstr = newstr.replace(ekey, RULE_LINK[key].split("|")[1])
     return newstr
-
-def convert_text()
-
 
 
 def convert(f):
     """ Convert """
     print(f.name)
-    print(convert_line("** Test et olé"))
+    test()
+    with f.open(encoding="utf-8") as ff:
+        content = ff.readlines()
+        for line in content:
+            print(convert_line(line))
+            
+
+    
+def test():
+    print(convert_line("** Titre2"))
+    print(convert_line("* Titre 1"))
+    print(convert_line("*** Titre 3 et oula"))
+    print(convert_line("**** Titre 4Test et olé"))
+    print(convert_line("***** Titre 5"))
+    print(convert_line("*C'est en bold*"))
+    print(convert_line("* C'est pas du bold c'est un titre*"))
+    print(convert_line(" * C'est pas du bold 1*"))
+    print(convert_line(" *C'est pas du bold 2 *"))
+    print(convert_line("Du *bold avec du texte* dans un autre texte"))
+    print(convert_line("Blah blah _underligned here_"))
+    print(convert_line("Blah blah _underligned here_    ggsgsg"))
+    print(convert_line("[[/un/lien/vers/le/paradis.png]]"))
+    print(convert_line("[[ /un/lien/vers/le/paradis.png]]"))
+    print(convert_line("[[/un/lien/vers/le/paradis.png ]]"))
+    print(convert_line("du texte [[/un/lien/vers/le/paradis.png]] encore du texte"))
+
+    
+    
 
     
 if __name__ ==  "__main__":
@@ -68,6 +131,7 @@ if __name__ ==  "__main__":
         else:
             print(temp + ": Not a valid file")
     convert(myfile)
+    
     
 
 
