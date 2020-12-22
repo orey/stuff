32678849df2f2c8ae41685439fdf1d1999e371f2
Olivier Rey <rey.olivier@gmail.com>
2020-12-22 17:34:27 +0100

    ok

--- dev/js-thegoodparts/promise.js
+++ dev/js-thegoodparts/promise.js
@@ -3,7 +3,7 @@
 let toto = 12;
 
 let p = new Promise((resolve, reject) => {
-    if (toto == 13){
+    if (toto == 12){
         console.log("resolved");
         resolve(toto);
     }
@@ -14,8 +14,20 @@ let p = new Promise((resolve, reject) => {
     }
 });
 
-p.then(param => console.log("Dans le then: " + param))
-    .catch(truc => console.log("Dans le catch: " + truc));
+p.then(param => {
+    console.log("Dans le then: " + param);
+    return {a : 1, b : 2};
+})
+    .catch(truc => {
+        console.log("Dans le catch: " + truc);
+        throw new Error("pour le suivant");
+    })
+    .then(obj => {
+        console.log("Au deuxième niveau");
+        console.log(JSON.stringify(obj));
+    })
+    .catch(err => console.log("Second niveau: " + err));
+          
 
 
 
