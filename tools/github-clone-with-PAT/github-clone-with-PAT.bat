8462e6debc85b191a69ca6c6cf6fce7c7f42d38a
orey <rey.olivier@gmail.com>
2021-06-02 07:29:47 +0200

    correction pac to pat

--- tools/github-clone-with-PAT/github-clone-with-PAT.bat
+++ tools/github-clone-with-PAT/github-clone-with-PAT.bat
@@ -2,12 +2,12 @@ cls
 @echo off
 
 if "%1" == "" (
-    echo Github clone script using PAC
+    echo Github clone script using PAT
     echo Usage : clone-github.bat [name_of_repo]
     goto :end
 )
 
-set /p pac=<PAC.txt
+set /p pac=<PAT.txt
 git clone https://orey:%pac%@github.com/orey/%1.git
 
 :end
