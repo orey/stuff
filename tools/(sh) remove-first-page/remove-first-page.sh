49353a02b870f17535b629e172a82eead17bc2bf
orey <rey.olivier@gmail.com>
2022-03-26 17:42:07 +0100

    new tool

---
+++ tools/(sh) remove-first-page/remove-first-page.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+
+if [ $# != 1 ]
+then
+    echo "remove-first-page.sh: Script version 1"
+    echo "Install pdftk first"
+    echo "Usage : remove-first-page.sh [file.pdf]"
+    exit 0
+fi
+
+temp="$1"
+nbofpages=$(pdfinfo "$1" | grep -Po 'Pages:[[:space:]]+\K[[:digit:]]+')
+
+pdftk A="$1" cat 2-$nbofpages output temp.pdf
+
+mv temp.pdf $1
+
+echo "Done"
+
