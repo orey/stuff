17c437d056f52da566bf715b02f73d5a952160dc
orey <rey.olivier@gmail.com>
2022-08-17 00:51:03 +0200

    new

---
+++ tools/(sh) insert-blank-page-in-pdf/insert-blank-page-a5-at.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# source: https://unix.stackexchange.com/questions/15992/how-do-i-insert-a-blank-page-into-a-pdf-with-ghostscript-or-pdftk
+
+convert xc:none -page A5 blank.pdf
+
+if [ $# -ne 3 ]
+then
+  echo "Usage example: ./pdfInsertBlankPageAt 5 src.pdf res.pdf"
+  exit $E_BADARGS
+else
+  pdftk A=$2 B=blank.pdf cat A1-$(($1-1)) B1 A$1-end output $3
+fi
+
