65c73ebe156b8b4658ba6c5eff4f5dc426922b2d
orey <rey.olivier@gmail.com>
2022-08-20 15:47:17 +0200

    new tool

---
+++ tools/(sh) insert-blank-page-in-pdf/insert-blank-page-at-in-same-doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# source: https://unix.stackexchange.com/questions/15992/how-do-i-insert-a-blank-page-into-a-pdf-with-ghostscript-or-pdftk
+
+convert xc:none -page A4 blank.pdf
+
+if [ $# -ne 2 ]
+then
+  echo "Usage example: ./insert-blank-page-at-same-doc src.pdf 12"
+  exit $E_BADARGS
+else
+  pdftk A=$1 B=blank.pdf cat A1-$(($2-1)) B1 A$2-end output temp_hfgdtsgjh.pdf
+fi
+
+mv temp_hfgdtsgjh.pdf $1
+
+
