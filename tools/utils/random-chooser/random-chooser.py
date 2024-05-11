0058a8ddd678d20ea3dd3550eff8dda4dabdefeb
orey <rey.olivier@gmail.com>
2024-05-11 18:47:50 +0200

    small enhancements to random-chooser

--- tools/utils/random-chooser/random-chooser.py
+++ tools/utils/random-chooser/random-chooser.py
@@ -19,11 +19,11 @@ def printHeader(dict):
 def printText(elem):
     print("-------------------------------")
     if CAT in elem:
-        print("Category: " + elem[CAT])
+        print("| Category    | " + elem[CAT])
     if SUB in elem:
-        print("Subcategory: " + elem[SUB])
+        print("| Subcategory | " + elem[SUB])
     if TEXT in elem:
-        print("Text: " + elem[TEXT])
+        print("| Text        | " + elem[TEXT])
     else:
         print("Strange, there should be a text in the element below...")
         print(elem)
@@ -39,16 +39,20 @@ def main():
         print("Found " + str(l) + " elements in the list")
         resp = input("Are you ready? [y/n] ")
         reste = True
-        if resp.upper() in ["Y","YES"]:
+        if resp.upper() in ["Y","YES",""]:
             while reste:
                 rank = random.choice(range(0,l))
                 elem = elems[rank]
                 printText(elem)
                 del elems[rank]
                 l = len(elems)
+                if l == 0:
+                    print("You've been through the full ist, bravo and goodbye!")
+                    input("Press return to exit")
+                    return
                 print(str(l) + " elements remaining in the list")
                 res = input("Continue? [y/n] ")
-                if res.upper() not in ["Y","YES"]:
+                if res.upper() not in ["Y","YES",""]:
                     reste = False
                     print("Goodbye !")
 
