4b09610862f2892e5920e2565e7a2111a58f9307
orey <rey.olivier@gmail.com>
2024-05-11 09:44:33 +0200

    new tool random chooser

---
+++ tools/utils/random-chooser/random-chooser.py
@@ -0,0 +1,61 @@
+import random
+import json
+
+FILENAME = 'list.json'
+
+# Grammar
+NAME ="name"
+VERSION = "version"
+LIST = "list"
+CAT = "category"
+SUB = "subcategory"
+TEXT = "text"
+
+
+def printHeader(dict):
+    print("Name: " + dict[NAME])
+    print("Version: " + dict[VERSION])
+
+def printText(elem):
+    print("-------------------------------")
+    if CAT in elem:
+        print("Category: " + elem[CAT])
+    if SUB in elem:
+        print("Subcategory: " + elem[SUB])
+    if TEXT in elem:
+        print("Text: " + elem[TEXT])
+    else:
+        print("Strange, there should be a text in the element below...")
+        print(elem)
+    print("-------------------------------")
+
+    
+def main():
+    with open(FILENAME) as f:
+        dict = json.load(f)
+        printHeader(dict)
+        elems = dict[LIST]
+        l = len(elems)
+        print("Found " + str(l) + " elements in the list")
+        resp = input("Are you ready? [y/n] ")
+        reste = True
+        if resp.upper() in ["Y","YES"]:
+            while reste:
+                rank = random.choice(range(0,l))
+                elem = elems[rank]
+                printText(elem)
+                del elems[rank]
+                l = len(elems)
+                print(str(l) + " elements remaining in the list")
+                res = input("Continue? [y/n] ")
+                if res.upper() not in ["Y","YES"]:
+                    reste = False
+                    print("Goodbye !")
+
+                
+        
+
+
+if __name__ == '__main__':
+    main()
+    
