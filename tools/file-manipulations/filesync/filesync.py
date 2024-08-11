5cbf364f8b064e81f3a61885a6ebc775ffafde8a
Olivier Rey <rey.olivier@gmail.com>
2024-08-11 11:21:57 +0200

    new version of the tool

--- tools/file-manipulations/filesync/filesync.py
+++ tools/file-manipulations/filesync/filesync.py
@@ -1,26 +1,40 @@
-import os, sys, shutil
+###########################################################
+# Synchronize folders and thier content
+# rey.olivier@gmail.com
+# August 11 2024
+###########################################################
+import os, sys, shutil, getopt
 import hashlib
 from time import time
 from datetime import datetime
 
-GARBAGE = '/tmp/filesync_'
-TIMESTAMP = datetime.now().strftime('%Y%m%d__%H%M%S-%f')
+# Global constants
+GARBAGE = '/tmp/'
+TIMESTAMP = 'filesync_' + datetime.now().strftime('%Y%m%d_%H%M%S-%f')
+
+# Works with interrupt
+DEBUG = False
 
 
 #============================================ interrupt
-def interrupt(text):
+def interrupt(text=""):
     '''
     Manual breakpoint
     '''
-    print(text)
-    resp = input("Continue? ")
-    if resp.upper() in ["N","NO"]:
-        print("Goodbye!")
-        exit(0)
+    if DEBUG == True:
+        if text != "":
+            print(text)
+        resp = input("Continue? ")
+        if resp.upper() in ["N","NO"]:
+            print("Goodbye!")
+            exit(0)
 
 
-# Unique footprint of the file
+#============================================ sha256sum
 def sha256sum(completefilename):
+    '''
+    Unique footprint of the file
+    '''
     h  = hashlib.sha256()
     b  = bytearray(128*1024)
     mv = memoryview(b)
@@ -30,17 +44,19 @@ def sha256sum(completefilename):
     return h.hexdigest()
 
 
+#============================================ ensure folder
 def ensure_folder(folder):
     if not os.path.isdir(folder):
         os.mkdir(folder)
 
         
-# Garbaging file is just moving it for safety
+#============================================ garbage
 def garbage(filename):
-    garb = GARBAGE + TIMESTAMP
+    # Garbaging file is just moving it for safety
+    garb = os.path.join(GARBAGE, TIMESTAMP)
     if not os.path.isdir(garb):
         os.mkdir(garb)
-        print(garb + " created")
+        print("Garbage folder " + garb + " created")
     timestamp = str(time()).split('.')[0] + str(time()).split('.')[1]
     #get just filename
     fil = filename.split('/')[-1]
@@ -50,9 +66,11 @@ def garbage(filename):
         print(err)
 
 
-# returns an dict       
+#============================================ analyze source
 def analyze_source(path):
-    #creating a dict with key = relativepath/filename.ext : value = full path
+    '''
+    Creates a dict with {relativepath/filename.ext : fullpath/filename.ext}
+    '''
     dict = {}
     count = 0
     for root, dirs, files in os.walk(path):
@@ -66,62 +84,114 @@ def analyze_source(path):
             #interrupt(dict)
             sys.stdout.write(str(count) + '|')
             sys.stdout.flush()
-    print("\n--- " + str(count) + " files found")
+    print("\n--- " + str(count) + " files found in " + path)
     return dict
 
 
-def analyze_target(path, sourcedict):
+#============================================ analyze target
+def analyze_target(path, sourcedict, analyze=False):
+    '''
+    We are treating the target while analyzing it
+    '''
     count = 0
     for root, dirs, files in os.walk(path):
-        print(dirs)
+        if DEBUG: print(dirs)
         for thefile in files: 
             count +=1
             completefilename = os.path.join(root,thefile)
             relativefilename = completefilename.split(path)[1]
-            # the objective is to see if this file is already at the "same" place in
-            # the local folder
             try:
+                # 1. Skip the identical files located at the same place
                 completesourcefilename = sourcedict[relativefilename]
                 print("Same file at the same place: " + relativefilename)
-                interrupt(completesourcefilename)
+                interrupt()
                 # Remove from sourcedict
                 del sourcedict[relativefilename]
             except KeyError:
-                print(sys.exception())
-                print("File exists in remote but no more locally. Putting the file in the garbage.")
-                garbage(completefilename)
-                interrupt("File garbaged: " + completefilename)
+                # 2. Remove target files that are not in the source
+                #print(sys.exception())
+                if analyze:
+                    print("To remove from target: " + relativefilename)
+                else:
+                    print("File existing in target but not in source. Garbaging: " + completefilename)
+                    interrupt()
+                    garbage(completefilename)
+    # 3. Create target files (and folders) based on existing source files that are not in target
     for key,value in sourcedict.items():
         filetocreate = os.path.join(path,key)
-        interrupt("File to create: " + filetocreate)
         # check if folders exist
         lis = key.replace(key.split('/')[-1], "").split('/')
-        if len(lis) > 1:
+        if len(lis) > 1 and not analyze:
             cumul = path
             for i in range(0, len(lis)-1):
                 ensure_folder(os.path.join(cumul,lis[i]))
                 cumul = os.path.join(cumul,lis[i])
-                print(cumul)
+                #print(cumul)
         interrupt(lis)
-        
-        shutil.copyfile(value, filetocreate)
-        interrupt("Voir les drives")
+        if analyze:
+            print("To create in target: " + filetocreate)
+        else:
+            shutil.copyfile(value, filetocreate)
+            interrupt("File " + filetocreate + " created in target")
+    # 4. Remove the potential void folders in target
+    for root, dirs, files in os.walk(path):
+        if len(os.listdir(root)) == 0:
+            interrupt("Folder is void and will be suppressed: " + root)
+            try:
+                shutil.rmtree(root)
+            except:
+                print("The folder " + root + "is not existing anymore so cannot be removed...")
 
-                      
-                
-    
 
+#============================================ usage
+def usage():
+    print("Synchronize 2 folders and their contents")
+    print("Usage: \n $ filesync -s [SOURCE_FOLDER] -t [TARGET_FOLDER] (-a) (-h)")
+    print("-SOURCE_FOLDER and TARGET_FOLDER are mandatory parameters")
+    print("-'-a' is just doing an analysis of the deltas")
+    print("-'-h' is for help")
+    sys.exit(0)    
 
-
-
-        
+#============================================ analyze target
 def main():
-    local = "/home/olivier/JDR-RPG/JDR/Ambre/"
-    remote = "/run/user/1000/gvfs/ftp:host=ls-wxl271.local/array1/DisqueBuffaloRaid1/JDR-RPG/JDR/Ambre/"
-    dict = analyze_source(local)
-    analyze_target(remote, dict)
-
-    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "s:t:g:adh",
+                                   ["source=", "target=", "garbage=", "analyze", "debug", "help"])
+    except getopt.GetoptError:
+        usage()
+    analyzemode = False
+    garbage = ""
+    source = ""
+    target = ""
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        if o in ("-d", "--debug"):
+            print("Debug mode activated")
+            global DEBUG
+            DEBUG = True
+        if o in ("-a", "--analyze"):
+            analyzemode = True
+        if o in ("-g", "--garbage"):
+            if not os.path.isdir(a):
+                print("Garbage folder '" + a + "' does not exist. Maybe there is a typo in the name. Exiting.")
+                sys.exit()
+            global GARBAGE
+            GARBAGE = a
+        if o in ("-s", "--source"):
+            if not os.path.isdir(a):
+                print("Source folder '" + a + "' does not exist. Maybe there is a typo in the name. Exiting.")
+                sys.exit()
+            source = a
+        if o in ("-t", "--target"):
+            if not os.path.isdir(a):
+                print("Target folder '" + a + "' does not exist. Maybe there is a typo in the name. Exiting.")
+                sys.exit()
+            target = a   
+    #local = "/home/olivier/JDR-RPG/JDR/Ambre/"
+    #remote = "/run/user/1000/gvfs/ftp:host=ls-wxl271.local/array1/DisqueBuffaloRaid1/JDR-RPG/JDR/Ambre/"
+    dict = analyze_source(source)
+    analyze_target(target, dict, analyzemode)
 
     
 if __name__ == "__main__":
