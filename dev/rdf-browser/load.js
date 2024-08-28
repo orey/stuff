f0bdac49eb02ba4e4db3c25d1e70df78267540d4
Olivier Rey <rey.olivier@gmail.com>
2024-08-28 13:41:14 +0200

    toy uite

--- dev/rdf-browser/load.js
+++ dev/rdf-browser/load.js
@@ -47,7 +47,7 @@ function within(x, y) {
 }
 
 function move(e) {
-    if (selection) {
+    if (selection && e.buttons) {
         selection.x = e.x;
         selection.y = e.y;
         draw();
@@ -56,25 +56,62 @@ function move(e) {
 
 function down(e) {
     let target = within(e.x, e.y);
+    if (selection && selection.selected) {
+        selection.selected = false;
+    }
     if (target) {
+        if (selection && selection !== target) {
+            edges.push({ from: selection, to: target });
+        }
         selection = target;
+        selection.selected = true;
+        draw();
     }
 }
 
 function up(e) {
-    selection = undefined;
+    if (!selection) {
+        let node = {
+            x: e.x,
+            y: e.y,
+            radius: 10,
+            fillStyle: '#22cccc',
+            strokeStyle: '#009999',
+            selectedFill: '#88aaaa',
+            selected: false
+        };
+        nodes.push(node);
+        draw();
+    }
+    if (selection && !selection.selected) {
+        selection = undefined;
+    }
+    draw();
 }
 
 window.onmousemove = move;
 window.onmousedown = down;
 window.onmouseup = up;
 
+var edges = [];
+
 function draw() {
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    for (let i = 0; i < edges.length; i++) {
+        let fromNode = edges[i].from;
+        let toNode = edges[i].to;
+        context.beginPath();
+        context.strokeStyle = fromNode.strokeStyle;
+        context.moveTo(fromNode.x, fromNode.y);
+        context.lineTo(toNode.x, toNode.y);
+        context.stroke();
+    }
+
     for (let i = 0; i < nodes.length; i++) {
         let node = nodes[i];
         context.beginPath();
-        context.fillStyle = node.fillStyle;
+        context.fillStyle = node.selected ? node.selectedFill : node.fillStyle;
         context.arc(node.x, node.y, node.radius, 0, Math.PI * 2, true);
         context.strokeStyle = node.strokeStyle;
         context.fill();
